c6d56171ef4b06f967faa235f1fbdc274befa59a
borshik22845-design <borshik22845@gmail.com>
2026-07-11 18:15:03 +1000

    Update Shuba_BD.py

--- Shuba_BD.py
+++ Shuba_BD.py
@@ -1,9 +1,13 @@
 from sqlalchemy import Column, Integer, String, Boolean, JSON, select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
-    "postgresql+asyncpg://bothost_db_aa0d6b86edb2:QdCGKHjHFN83a2PTufS4kBO4fFeFbHjRwsK0ZsRzV5I@node1.pghost.ru:15863/bothost_db_aa0d6b86edb2",
+    DATABASE_URL,
     echo=False
 )
 
