f499c79d0ae348aa6f84dbee97ec805cf8accd21
borshik22845-design <borshik22845@gmail.com>
2026-07-11 20:57:58 +1000

    Update Shuba_BD.py

--- Shuba_BD.py
+++ Shuba_BD.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("🔍 ТЕКУЩИЙ URL БАЗЫ:", DATABASE_URL)  # <-- ДОБАВЬ ЭТУ СТРОКУ
+print("🔍 ТЕКУЩИЙ URL БАЗЫ:", DATABASE_URL)
 
 engine = create_async_engine(
     DATABASE_URL,
