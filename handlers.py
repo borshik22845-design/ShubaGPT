8c1d0586bdb500a3eb240d5f2a1aad2bd6bd5abd
borshik22845-design <borshik22845@gmail.com>
2026-07-30 18:24:51 +1000

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -75,7 +75,7 @@ async def cmd_ai_photo(message: Message, client: AsyncOpenAI, bot: Bot):
                 photo = message.photo[-1]
                 caption = message.caption or ''
                 file = await bot.download(photo.file_id)
-                user_message = await process_photo(file, photo, caption)
+                user_message = await process_photo(file, caption)
                 async with ChatActionSender.typing(chat_id=message.chat.id, bot=bot):
                     try:
                         assistant_reply = await process_ai(client, user_id, user_message, name)
@@ -92,7 +92,7 @@ async def cmd_ai_photo(message: Message, client: AsyncOpenAI, bot: Bot):
             name = message.from_user.first_name or message.from_user.username or "Пользователь"
             photo = message.photo[-1]
             caption = message.caption or ''
-            user_message = await process_photo(bot, photo, caption)
+            user_message = await process_photo(file, caption)
             async with ChatActionSender.typing(chat_id=message.chat.id, bot=bot):
                 try:
                     assistant_reply = await process_ai(client, user_id, user_message, name)
