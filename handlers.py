ce13d058e0688a0aaa6d53e919e667587bf8c6dd
borshik22845-design <borshik22845@gmail.com>
2026-07-30 18:15:13 +1000

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -74,7 +74,8 @@ async def cmd_ai_photo(message: Message, client: AsyncOpenAI, bot: Bot):
                 name = message.from_user.first_name or message.from_user.username or "Пользователь"
                 photo = message.photo[-1]
                 caption = message.caption or ''
-                user_message = await process_photo(bot, photo, caption)
+                file = await bot.download(photo.file_id)
+                user_message = await process_photo(file, photo, caption)
                 async with ChatActionSender.typing(chat_id=message.chat.id, bot=bot):
                     try:
                         assistant_reply = await process_ai(client, user_id, user_message, name)
