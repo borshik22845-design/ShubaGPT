0834e0e91c69399f84c5a6b3904331ab88b97dc0
borshik22845-design <borshik22845@gmail.com>
2026-08-06 10:10:50 +1000

    Update db.py

--- db.py
+++ db.py
@@ -5,7 +5,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("🔍 ТЕКУЩИЙ URL БАЗЫ:", DATABASE_URL)
 
 engine = create_async_engine(
     DATABASE_URL,
